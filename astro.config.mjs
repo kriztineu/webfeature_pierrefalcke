781da2ee2130554ef6cfa888c10e6ba50dd7220e
Kristine Urhammer <kristine.urhammer@live.dk>
2023-09-07 12:00:45 +0200

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,4 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 
-export default defineConfig({
-  experimental: {
-    assets: true,
-  },
-});
+export default defineConfig({});
